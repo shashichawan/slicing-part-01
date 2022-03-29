ad1f8605cc47bf79d141c7efc75b001c7f1914bb
shashichawan <102402158+shashichawan@users.noreply.github.com>
2022-03-29 12:06:34 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,133 @@
+#print("hello, world")#hello, world
+#print("hello world")#hello world
+#print("hello"+''+"world")#helloworld
+#print("hello""world",'core','python',sep="")#helloworldcorepython
+#print("hello","world",'core','python',sep="-")#hello-world-core-python
+#print("hello","world",'core','python',sep="-",)#hello-world-core-python
+#print("hello","world",'core','python',sep="-""ok")#hello-okworld-okcore-okpython
+#print("hello","world",'core','python',sep="-"''"ok")#hello-okworld-okcore-okpython
+
+# print("hello","python",end=".")#hellopython.hello-pythonnew version is 3.10.3ok fine
+# print("hello","python",end="-")
+# print("new version is 3.10.3")
+# print("ok fine")
+
+# print("happy","ugadi",end=".",sep='')
+# print("happy","ugadi",end=".",sep='-')
+# print("happy","ugadi",sep="@",ene="#")#happyugadi.happy-ugadi.happy@ugadi#
+
+#print("hello world")#hello world
+
+
+
+
+
+# a=input()
+# print(a,type(a))
+# print("the value of a is"+a)
+
+# b=input()
+# print(b,type(b))
+# print("the value of b is"+b)
+
+# c=int(input("enter your phne number:"))
+# print(c,type(c))
+# print("the value of c is",c)
+
+# d=float(input("enter your height:"))
+# print(c,type(c))
+# print("the value of d is",d)
+
+# e=str(input("enter your weight:"))
+# print(e,type(e))
+# print("the value of d is",e)
+
+# a=input('enter your name:')
+# print('my name is',a)
+# print("my name is",a,sep="-")
+# print("my name is",int(a))
+# print("my name is",float(a))
+# print("my name is",str(a))
+# print("my name is"+''+str(a))
+# print("my name is"+' '+a,)
+
+# name=input("enter your name:")
+# age=int(input("enter your age:"))
+# per=float(input("enter your percentage:"))
+
+# print(name ,age,per)
+# print(age,name,per)
+
+# print("your name is,"name",and age is","age",\
+#     "graduation percentage is",per)
+
+# print("your name is{}and age is{}.\
+#     "graduation percentage is{}".format(name,age,per))
+
+# print(f"your name is {name}and age is{age}.\
+#     graduation percentageis{per}")
+
+# from crypt import methods
+
+
+# slicing methods
+# print(len("python"))
+
+# a="python"
+# print(a,type(a))
+# print(a[0])
+# print("mahesh"[0])
+# print("mahesh"[1])
+# print("mahesh"[2])
+# print("mahesh"[3])
+# print("mahesh"[4])
+# print("mahesh"[5])
+# print("mahesh"[6])
+# print("mahesh"[7])
+# print('mahesh'[8])
+
+
+# b="department"
+# print(b[3]+b[2]+b[3]+b[4]+b[5])
+
+# print("python"[-1])
+# print("python"[-2])
+# print("python"[-3])
+# print("python"[-4])
+# print("python"[-5])
+# print("python"[-6])
+# print("python"[-7])
+
+e="python"
+# print(e[:])
+# print(e[0:])
+# print(e[0:6])
+# print(e[0:0])
+# print(e[0:1])
+# print(e[1:1])
+# print(e[1:0])
+# print(e[0:2])
+# print(e[0:3])
+# print(e[0:4])
+# print(e[0:5])
+# print(e[0:6])
+# print(e[0:7])
+# print(e[0:99])
+# print(e[0:0])
+# print(e[2:3])
+# print(e[5:3])
+# print(e[0:-1])
+# print(e[0:-2])
+# print(e[0:-3])
+# print(e[0:-4])
+# print(e[0:-5])
+# print(e[0:-6])
+# print(e[0:-7])
+
+# d="python"
+# print(d[2:5])
+# print(d[3:5])
+# print(d[5:2])
+# print(d[4:5])
+
+# print(d[-6:0])
